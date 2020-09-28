650312cac59f8a51053823227f68cb9a35a9300c
Jakub Gruber <jgr@paymorrow.com>
2020-09-28 11:07:28 +0200

    Added Sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import './styles/App.scss';
 
 const App = () => <div>Hello React App!</div>;
 
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
             {
                 test: /\.tsx?$/,
                 loader: "ts-loader"
+            },
+            {
+                test:/\.(s*)css$/,
+                use:['style-loader','css-loader', 'sass-loader']
             }
         ]
     },
