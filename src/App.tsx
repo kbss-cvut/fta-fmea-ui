d619625ca991c375efedd9cf452e32479489bc2f
Jakub Gruber <jgr@paymorrow.com>
2020-10-02 19:54:37 +0200

    Simple Login Dialog UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
 import * as React from "react";
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {HashRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 import '@styles/App.scss';
 import Home from "@components/Home";
 import Login from "@components/Login"
 
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core";
+
+// TODO configure global theme
+const theme = createMuiTheme({
+    overrides: {
+
+    }
+});
+
 const App = () => {
     return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/login">Login</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path="/login">
-                        <Login/>
-                    </Route>
-                    <Route path="/">
-                        <Home/>
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
+        <MuiThemeProvider theme={theme}>
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Switch>
+                        <Route path="/login">
+                            <Login/>
+                        </Route>
+                        <Route path="/">
+                            <Home/>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </MuiThemeProvider>
     );
 }
 
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,51 @@
 import * as React from "react";
+import {makeStyles, Button, Paper, TextField, Grid, FormControlLabel, Checkbox, Box} from "@material-ui/core";
+import {Face} from '@material-ui/icons'
+
+const useStyles = makeStyles((theme) => ({
+    margin: {
+        margin: theme.spacing(2),
+    },
+    padding: {
+        padding: theme.spacing(1)
+    },
+    box: {
+        display: 'flex'
+    },
+    paper: {
+        elevation: 3,
+        padding: theme.spacing(3),
+        margin: 'auto'
+    }
+}));
 
 const Login = () => {
+    const classes = useStyles();
     return (
-        <form method="post">
-            <h2>Log in</h2>
-            <input type="text" className="form-control" placeholder="Username"/>
-            <input type="password" placeholder="Password"/>
-            <button type="submit">Log in</button>
-        </form>
+        <Box className={classes.box}>
+            <Paper className={classes.paper}>
+                <div className={classes.margin}>
+                    <Grid container justify="center">
+                        <Grid item>
+                            <Face/>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={8} alignItems="flex-end">
+                        <Grid item md={true} sm={true} xs={true}>
+                            <TextField id="username" label="Username" type="email" fullWidth autoFocus required/>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={8} alignItems="flex-end">
+                        <Grid item md={true} sm={true} xs={true}>
+                            <TextField id="username" label="Password" type="password" fullWidth required/>
+                        </Grid>
+                    </Grid>
+                    <Grid container justify="center" style={{marginTop: '10px'}}>
+                        <Button variant="outlined" color="primary" style={{textTransform: "none"}}>Login</Button>
+                    </Grid>
+                </div>
+            </Paper>
+        </Box>
     );
 }
 
