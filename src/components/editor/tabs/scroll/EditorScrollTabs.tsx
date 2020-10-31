da8713f8fa586bee369e403fa0a3fffc7a68744b
Jakub Gruber <jgr@paymorrow.com>
2020-10-31 17:10:39 +0100

    Fix closing tabs

--- src/components/editor/tabs/scroll/EditorScrollTabs.tsx
+++ src/components/editor/tabs/scroll/EditorScrollTabs.tsx
@@ -6,7 +6,17 @@ import {useOpenTabs} from "@hooks/useOpenTabs";
 import {FaultEvent} from "@models/eventModel";
 import TabPanel from "@components/editor/tabs/panel/TabPanel";
 import Editor from "@components/editor/Editor";
-import CloseableTab, {a11yProps} from "@components/editor/tabs/CloseableTab";
+import {IconButton} from "@material-ui/core";
+import {Close} from "@material-ui/icons";
+import Tab from "@material-ui/core/Tab";
+import {FailureMode} from "@models/failureModeModel";
+
+const a11yProps = (index: any) => {
+    return {
+        id: `scrollable-auto-tab-${index}`,
+        'aria-controls': `scrollable-auto-tabpanel-${index}`,
+    };
+}
 
 const EditorScrollTabs = () => {
     const classes = useStyles();
@@ -17,6 +27,10 @@ const EditorScrollTabs = () => {
     };
 
     const [openTabs, _, closeTab] = useOpenTabs();
+    const handleTabClose = (index: number, failureMode: FailureMode) => {
+        setCurrentTab(index - 1)
+        closeTab(failureMode)
+    }
 
     return (
         <div className={classes.root}>
@@ -33,8 +47,11 @@ const EditorScrollTabs = () => {
                     {
                         openTabs.map((failureMode, index) => {
                             const tabTitle = (failureMode.manifestingNode.event as FaultEvent).name
-                            return <CloseableTab title={tabTitle} a11yProps={a11yProps(index)}
-                                                 onTabClose={() => closeTab(failureMode)}/>
+                            return <Tab
+                                component="div"
+                                label={<span>{tabTitle}<IconButton
+                                    onClick={() => handleTabClose(index, failureMode)}><Close/></IconButton></span>}
+                                {...a11yProps(index)}/>
                         })
                     }
                 </Tabs>
