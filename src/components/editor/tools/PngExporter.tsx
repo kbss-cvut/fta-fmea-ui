4076974464ae3f5f9308b08f49e5a2e34ada6a45
Jakub Gruber <jgr@paymorrow.com>
2020-11-06 12:39:43 +0100

    SVG Export - almost done

--- src/components/editor/tools/PngExporter.tsx
+++ src/components/editor/tools/PngExporter.tsx
@@ -3,7 +3,8 @@ import {Button, Dialog} from "@material-ui/core";
 import {DialogTitle} from "@components/dialog/custom/DialogTitle";
 import {DialogContent} from "@components/dialog/custom/DialogContent";
 import {DialogActions} from "@components/dialog/custom/DialogActions";
-import {useEffect, useState} from "react";
+import {useState} from "react";
+import useStyles from "@components/editor/tools/PngExporter.styles";
 
 export interface PngExportData {
     encodedData: any,
@@ -18,42 +19,55 @@ interface Props {
 }
 
 const PngExporter = ({exportData, open, onClose}: Props) => {
+    const classes = useStyles();
+
     const {encodedData, width, height} = exportData
     const b64Start = 'data:image/svg+xml;base64,';
     const imageData = b64Start + encodedData;
 
-    const [canvasRef, setCanvasRef] = useState<HTMLCanvasElement>()
+    const [canvasRef, setCanvasRef] = useState<HTMLCanvasElement>(null);
+    const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
 
-    // useEffect(() => {
-    //     console.log('useEffect')
-    //     if (canvasRef) {
-    //         // @ts-ignore
-    //         const context = canvasRef.getContext('2d');
-    //
-    //         const image = new Image(width, height);
-    //         image.src = imageData;
-    //         // @ts-ignore
-    //         image.onload = () => {
-    //             console.log('onLoad')
-    //             context.drawImage(image, 0, 0, width, height);
-    //             forceRender(true)
-    //         };
-    //     }
-    // }, [canvasRef])
+    React.useEffect(() => {
+        if (canvasRef) {
+            const renderCtx = canvasRef.getContext('2d');
 
-    console.log('render')
+            if (renderCtx) {
+                setContext(renderCtx);
+            }
+        }
+    }, [canvasRef]);
+
+    if (context) {
+        const img = new Image(width, height);
+        img.src = imageData;
+        img.onload = () => {
+            context.drawImage(img, 0, 0, width, height);
+        };
+    }
+
+    const handleExport = () => {
+        const png = canvasRef.toDataURL('image/png')
+            .replace("image/png", "image/octet-stream");
+        document.getElementById("download").setAttribute('href', png);
+    }
 
     return (
         <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title" fullScreen>
             <DialogTitle id="form-dialog-title" onClose={onClose}>Export Diagram</DialogTitle>
             <DialogContent dividers>
-                <img width={width} height={height} src={imageData}/>
-                {/*<canvas width={width} height={height} ref={ref => setCanvasRef(ref)}/>*/}
+                <canvas
+                    ref={ref => setCanvasRef(ref)}
+                    width={width}
+                    height={height}
+                    className={classes.canvas}
+                />
             </DialogContent>
             <DialogActions>
-                <Button color="primary">
+                <Button color="primary" onClick={handleExport}>
                     Export & Save
                 </Button>
+                <a id="download" download="diagram.png">Download</a>
             </DialogActions>
         </Dialog>
     );
