3e9766be980d4c268e35f59d5aab0bb359c53dce
Jakub Gruber <jgr@paymorrow.com>
2020-11-16 20:23:44 +0100

    Submit password change by enter

--- src/components/dialog/password/ChangePasswordDialog.tsx
+++ src/components/dialog/password/ChangePasswordDialog.tsx
@@ -12,14 +12,12 @@ import {SnackbarType, useSnackbar} from "../../../hooks/useSnackbar";
 import {schema} from "./ChangePasswordDialog.schema";
 
 const ChangePasswordDialog = ({open, handleCloseDialog, user}) => {
-    const [processing, setIsProcessing] = useState(false)
     const [showSnackbar] = useSnackbar();
 
-    const {register, handleSubmit, errors} = useForm({resolver: yupResolver(schema),});
+    const {register, handleSubmit, errors, formState} = useForm({resolver: yupResolver(schema),});
+    const {isSubmitting} = formState
 
     const handleChangePassword = async (values: any) => {
-        setIsProcessing(true)
-
         userService.changePassword({
             uri: user.iri,
             username: user.username,
@@ -28,31 +26,30 @@ const ChangePasswordDialog = ({open, handleCloseDialog, user}) => {
         }).then(value => {
             showSnackbar("User password updated", SnackbarType.SUCCESS);
             handleCloseDialog()
-        }).catch(reason => {
-            setIsProcessing(false)
-            showSnackbar(reason, SnackbarType.ERROR)
-        })
+        }).catch(reason => showSnackbar(reason, SnackbarType.ERROR))
     }
 
 
     return (
         <Dialog open={open} onClose={handleCloseDialog} aria-labelledby="form-dialog-title" maxWidth="md"
                 fullWidth>
-            <DialogTitle id="form-dialog-title" onClose={handleCloseDialog}>Change Password?</DialogTitle>
-            <DialogContent dividers>
-                <TextField inputRef={register} margin="normal" required fullWidth
-                           name="password" label="Current Password" type="password"
-                           error={!!errors.password}/>
+            <form onSubmit={handleSubmit(handleChangePassword)} noValidate>
+                <DialogTitle id="form-dialog-title" onClose={handleCloseDialog}>Change Password?</DialogTitle>
+                <DialogContent dividers>
+                    <TextField inputRef={register} margin="normal" required fullWidth
+                               name="password" label="Current Password" type="password"
+                               error={!!errors.password}/>
 
-                <TextField inputRef={register} margin="normal" required fullWidth
-                           name="newPassword" label="New Password" type="password"
-                           error={!!errors.newPassword}/>
-            </DialogContent>
-            <DialogActions>
-                <Button disabled={processing} color="primary" onClick={handleSubmit(handleChangePassword)}>
-                    Change Password
-                </Button>
-            </DialogActions>
+                    <TextField inputRef={register} margin="normal" required fullWidth
+                               name="newPassword" label="New Password" type="password"
+                               error={!!errors.newPassword}/>
+                </DialogContent>
+                <DialogActions>
+                    <Button disabled={isSubmitting} color="primary" type="submit">
+                        Change Password
+                    </Button>
+                </DialogActions>
+            </form>
         </Dialog>
     );
 }
