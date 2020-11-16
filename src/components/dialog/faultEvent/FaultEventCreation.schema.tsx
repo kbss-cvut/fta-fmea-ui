c66ddda8e1310cf696813b41ad9a9317e37bde30
Jakub Gruber <jgr@paymorrow.com>
2020-11-16 22:21:48 +0100

    RPN validation - inclusive bounds

--- src/components/dialog/faultEvent/FaultEventCreation.schema.tsx
+++ src/components/dialog/faultEvent/FaultEventCreation.schema.tsx
@@ -7,17 +7,17 @@ export const schema = Yup.object().shape({
         .required('Event is mandatory'),
     description: Yup.string().default(''),
     probability: Yup.number()
-        .moreThan(0, 'Probability cannot be lower than 0')
-        .lessThan(1, 'Probability cannot be greater than 1')
+        .min(0, 'Probability cannot be lower than 0')
+        .max(1, 'Probability cannot be greater than 1')
         .required('Probability is mandatory'),
     severity: Yup.number()
         .transform(yupOptionalNumber)
         .notRequired()
-        .moreThan(0, 'Severity cannot be lower than 0')
-        .lessThan(10, 'Severity cannot be greater than 10'),
+        .min(0, 'Severity cannot be lower than 0')
+        .max(10, 'Severity cannot be greater than 10'),
     detection: Yup.number()
         .transform(yupOptionalNumber)
         .notRequired()
-        .moreThan(0, 'Detection cannot be lower than 0')
-        .lessThan(10, 'Detection cannot be greater than 10')
+        .min(0, 'Detection cannot be lower than 0')
+        .max(10, 'Detection cannot be greater than 10')
 });
