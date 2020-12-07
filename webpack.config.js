237d2ee3f2db213c46661a3ce7f147977b085fc6
Jakub Gruber <jgr@paymorrow.com>
2020-12-07 16:54:53 +0100

    Add environments for dot env

--- webpack.config.js
+++ webpack.config.js
@@ -5,52 +5,56 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const path = require('path')
 
-module.exports = {
-    // Set debugging source maps to be "inline" for
-    // simplicity and ease of use
-    devtool: "inline-source-map",
+module.exports = env => {
+    return {
+        // Set debugging source maps to be "inline" for
+        // simplicity and ease of use
+        devtool: "inline-source-map",
 
-    mode: 'development',
+        mode: 'development',
 
-    // The application entry point
-    entry: "./src/index.tsx",
+        // The application entry point
+        entry: "./src/index.tsx",
 
-    // Where to compile the bundle
-    // By default the output directory is `dist`
-    output: {
-        path: __dirname + '/public',
-        filename: "build/bundle.js",
-        publicPath: "/"
-    },
+        // Where to compile the bundle
+        // By default the output directory is `dist`
+        output: {
+            path: __dirname + '/public',
+            filename: "build/bundle.js",
+            publicPath: "/"
+        },
 
-    devServer: {
-        historyApiFallback: true,
-        publicPath: "/"
-    },
+        devServer: {
+            historyApiFallback: true,
+            publicPath: "/"
+        },
 
-    // Supported file loaders
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader"
+        // Supported file loaders
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader"
+                }
+            ]
+        },
+
+        plugins: [
+            new ExtractTextPlugin({
+                filename: 'build/app.bundle.css',
+            }),
+            new Dotenv({
+                path: `./.env.${env.NODE_ENV === "prod" ? "prod" : "dev"}`,
+            })
+        ],
+
+        // File extensions to support resolving
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"],
+            plugins: [new TsconfigPathsPlugin({})],
+            alias: {
+                'jsonld': path.resolve('./node_modules/jsonld/dist/jsonld.js'),
             }
-        ]
-    },
-
-    plugins: [
-        new ExtractTextPlugin({
-            filename: 'build/app.bundle.css',
-        }),
-        new Dotenv()
-    ],
-
-    // File extensions to support resolving
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        plugins: [new TsconfigPathsPlugin({})],
-        alias: {
-            'jsonld': path.resolve('./node_modules/jsonld/dist/jsonld.js'),
         }
     }
 };
