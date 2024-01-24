14d38ea63255589e8d26be0c7abc5c8b0b454f7c
Max Chopart <max.chopart@gmail.com>
2024-01-24 09:06:26 +0100

    [New #151] Add tsconfigpath to resolve "@" paths

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import envCompatible from "vite-plugin-env-compatible";
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   base: "",
   plugins: [
     react(),
-    envCompatible()
+    envCompatible(),
+    tsconfigPaths()
   ],
   define: {
     "process.env": process.env
