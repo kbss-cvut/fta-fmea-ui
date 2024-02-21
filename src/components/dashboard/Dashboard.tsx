041b683a1845cd686e6b9bb5d45fb8500fdf7355
Kiryl Koniukh <49252228+Kasmadei@users.noreply.github.com>
2024-02-21 20:05:56 +0100

    Added i18-React

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,25 +1,28 @@
-import * as React from "react";
-import AppBar from "@components/appBar/AppBar";
-import DashboardContentProvider from "@hooks/DashboardContentProvider";
-import DashboardContent from "@components/dashboard/content/DashboardContent";
-import {CssBaseline} from "@mui/material";
-import Toolbar from "@mui/material/Toolbar";
-import useStyles from "@components/dashboard/Dashboard.styles";
+import * as React from 'react';
+import AppBar from '@components/appBar/AppBar';
+import DashboardContentProvider from '@hooks/DashboardContentProvider';
+import DashboardContent from '@components/dashboard/content/DashboardContent';
+import { CssBaseline } from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
+import useStyles from '@components/dashboard/Dashboard.styles';
+import { useTranslation } from 'react-i18next';
 
 const Dashboard = () => {
     const { classes } = useStyles();
 
+    const { t } = useTranslation();
+
     return (
         <DashboardContentProvider>
             <div className={classes.root}>
-                <CssBaseline/>
-                <AppBar title={'Dashboard'}/>
-                <Toolbar/>
+                <CssBaseline />
+                <AppBar title={t('main.appBarHeader')} />
+                <Toolbar />
 
-                <DashboardContent/>
+                <DashboardContent />
             </div>
         </DashboardContentProvider>
     );
-}
+};
 
 export default Dashboard;
