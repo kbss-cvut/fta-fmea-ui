353d76086f3f628c5fb3aa0a6649698e6e58c5bd
Kiryl Koniukh <49252228+Kasmadei@users.noreply.github.com>
2024-02-29 19:50:20 +0100

    Small fix

--- src/styles/App.styles.tsx
+++ src/styles/App.styles.tsx
@@ -13,21 +13,22 @@ export const appTheme = createCustomMuiTheme({
       strokeColor: "black",
     },
     editor: {
-        shape: {
-            padding: _defaultTheme.spacing(2) as unknown as number,
-            strokeWidth: 1,
-            strokeColor: 'black'
-        },
-        fontSize: 15
+      shape: {
+        padding: _defaultTheme.spacing(2) as unknown as number,
+        strokeWidth: 1,
+        strokeColor: "black",
+      },
+      fontSize: 15,
     },
     sidePanel: {
-        colors: {
-            icon: '#757575',
-            iconActive: '#3386D7',
-            text: '#212121',
-            textActive: '#1976D2DE',
-            hint: '#00000099',
-            hover: '#1976d21f'
-        }
-    }
+      colors: {
+        icon: "#757575",
+        iconActive: "#3386D7",
+        text: "#212121",
+        textActive: "#1976D2DE",
+        hint: "#00000099",
+        hover: "#1976d21f",
+      },
+    },
+  },
 });
