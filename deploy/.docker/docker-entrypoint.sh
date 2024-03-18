2cbc221b235d6eba15f237d5eeef8ef1558a12d8
Miroslav Blasko <blcham@gmail.com>
2024-03-18 22:46:46 +0100

    [Fix] Add missing processing of runtime var application context

--- deploy/.docker/docker-entrypoint.sh
+++ deploy/.docker/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${FTA_FMEA_API_URL} ${FTA_FMEA_ADMIN_REGISTRATION_ONLY} ${FTA_FMEA_TITLE}' < /etc/nginx/config.js.template > /usr/share/nginx/html/config.js
+envsubst '${FTA_FMEA_CONTEXT} ${FTA_FMEA_API_URL} ${FTA_FMEA_ADMIN_REGISTRATION_ONLY} ${FTA_FMEA_TITLE}' < /etc/nginx/config.js.template > /usr/share/nginx/html/config.js
 
 exec "$@"
