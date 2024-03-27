3a26998f49c4dd24cb2e7239f88c2f82e92e670c
Kiryl Koniukh <49252228+Kasmadei@users.noreply.github.com>
2024-03-27 20:03:33 +0100

    Updated i18n config (translation.json path fix)

--- src/i18n.js
+++ src/i18n.js
@@ -1,11 +1,17 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
+import translationEN from "../public/locales/en/translation.json";
+import translationCS from "../public/locales/cs/translation.json";
 
 i18n
   .use(Backend)
   .use(initReactI18next)
   .init({
+    resources: {
+      en: { translation: translationEN },
+      cs: { translation: translationCS },
+    },
     fallbackLng: "en",
     debug: true,
 
