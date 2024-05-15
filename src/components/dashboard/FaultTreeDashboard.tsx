0cc3fc2a56cdd92ce1ee6eed3902c0319abe4429
Max Chopart <chopart.max@proton.me>
2024-05-15 15:45:20 +0200

    [Upd] Removed/Refactored imports

--- src/components/dashboard/FaultTreeDashboard.tsx
+++ src/components/dashboard/FaultTreeDashboard.tsx
@@ -1,13 +1,9 @@
 import * as React from "react";
 import { useParams } from "react-router-dom";
-import AppBar from "../appBar/AppBar";
-import { CssBaseline } from "@mui/material";
-import Toolbar from "@mui/material/Toolbar";
 import useStyles from "./Dashboard.styles";
 import { composeFragment } from "@services/utils/uriIdentifierUtils";
 import { CurrentFaultTreeProvider } from "@hooks/useCurrentFaultTree";
 import Editor from "../editor/faultTree/Editor";
-import { useState } from "react";
 
 const FaultTreeDashboard = () => {
   const { classes } = useStyles();
