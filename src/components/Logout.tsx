0e3e3d0e1df239c45e31fd63f1b42ca112b37454
Max Chopart <chopart.max@proton.me>
2024-05-16 21:28:10 +0200

    [Upd #126] Add logout feature when logged in with OIDC

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,15 +1,21 @@
 import * as React from "react";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { User } from "@models/userModel";
 import { useLoggedUser } from "@hooks/useLoggedUser";
+import { isUsingOidcAuth } from "@utils/OidcUtils";
+import { AuthContext } from "@oidc/OidcAuthWrapper";
 
 const Logout = () => {
   const [_, setLoggedUser] = useLoggedUser();
-
+  const authCtx = useContext(AuthContext);
   useEffect(() => {
-    setLoggedUser({
-      authenticated: false,
-    } as User);
+    if (isUsingOidcAuth()) {
+      authCtx.logout();
+    } else {
+      setLoggedUser({
+        authenticated: false,
+      } as User);
+    }
   }, []);
 
   return <div>Logging out!</div>;
