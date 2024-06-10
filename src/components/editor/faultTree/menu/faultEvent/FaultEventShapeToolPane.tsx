b02a39a7f454f2a5a2028da1646da5d9da9565f3
Bogdan Kostov <kostobog@fel.cvut.cz>
2024-06-11 00:17:06 +0200

    [Fix #385] Avoid stack overflow exception by removing value of existingEvent from default values in update fault event form.

--- src/components/editor/faultTree/menu/faultEvent/FaultEventShapeToolPane.tsx
+++ src/components/editor/faultTree/menu/faultEvent/FaultEventShapeToolPane.tsx
@@ -40,11 +40,6 @@ const FaultEventShapeToolPane = ({ data, onEventUpdated, refreshTree }: Props) =
       probability: data.probability ? data.probability : 0.01,
       gateType: data.gateType ? data.gateType : null,
       sequenceProbability: data.sequenceProbability,
-      existingEvent: Array.isArray(data.supertypes)
-        ? data.supertypes.length > 0
-          ? data.supertypes[0]
-          : null
-        : data.supertypes,
     };
 
     useFormMethods = useForm({
