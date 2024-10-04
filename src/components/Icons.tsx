918400bf897dec23a11b01a7a94cf56c724998f9
Bogdan Kostov <kostobog@fel.cvut.cz>
2024-10-04 16:26:38 +0200

    Add message counter indicator to syncProblemIcon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -6,10 +6,18 @@ export const warnIcon = (message: any) => {
   return iconWithMessage(<Warning transform={"scale(0.88)"} />, message);
 };
 
-export const syncProblemIcon = (message: any) => {
-  return iconWithMessage(<SyncProblem transform={"scale(0.88)"} />, message);
+export const syncProblemIcon = (message: any, messageCount = 1) => {
+  return iconWithMessage(
+    <SyncProblem transform={"scale(0.88)"} />,
+    message,
+    messageCount && messageCount > 1 ? messageCount : null,
+  );
 };
 
-export const iconWithMessage = (icon, message: any) => {
-  return <Tooltip title={message}>{icon}</Tooltip>;
+export const iconWithMessage = (icon, message: any, label: string | number = "") => {
+  return (
+    <Tooltip title={message}>
+      {icon} {<span style={{ verticalAlign: "top", position: "fixed" }}>{label}</span>}
+    </Tooltip>
+  );
 };
