c420c78e2fb6982a7dc818ce792074f2a3875524
Bogdan Kostov <kostobog@fel.cvut.cz>
2024-10-08 09:53:25 +0200

    In Icons.tsx refactor common transform literal as a constant.

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -2,13 +2,15 @@ import { SyncProblem, Warning } from "@mui/icons-material";
 import { Tooltip } from "@mui/material";
 import React from "react";
 
+const transform = "scale(0.88)";
+
 export const warnIcon = (message: any) => {
-  return iconWithMessage(<Warning transform={"scale(0.88)"} />, message);
+  return iconWithMessage(<Warning transform={transform} />, message);
 };
 
 export const syncProblemIcon = (message: any, messageCount = 1) => {
   return iconWithMessage(
-    <SyncProblem transform={"scale(0.88)"} />,
+    <SyncProblem transform={transform} />,
     message,
     messageCount && messageCount > 1 ? messageCount : null,
   );
