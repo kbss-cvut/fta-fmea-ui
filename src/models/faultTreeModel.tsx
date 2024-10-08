a309e74ca6bfe2feaaff738a6c10703b545379a9
Bogdan Kostov <kostobog@fel.cvut.cz>
2024-10-08 13:00:28 +0200

    [Fix #593] Fix jsonld context for system in faultTreeModel.tsx

--- src/models/faultTreeModel.tsx
+++ src/models/faultTreeModel.tsx
@@ -3,18 +3,28 @@ import { FaultEvent, CONTEXT as EVENT_CONTEXT } from "@models/eventModel";
 import { AbstractModel, CONTEXT as ABSTRACT_CONTEXT } from "@models/abstractModel";
 import { FaultEventScenario, CONTEXT as SCENARIO_CONTEXT } from "@models/faultEventScenario";
 import { OperationalDataFilter, CONTEXT as FILTER_CONTEXT } from "@models/operationalDataFilterModel";
-import { System } from "@models/systemModel";
+import { System, CONTEXT as SYSTEM_CONTEXT } from "@models/systemModel";
 import { Status } from "@utils/constants";
 
 const ctx = {
   manifestingEvent: VocabularyUtils.PREFIX + "is-manifested-by",
   faultEventScenarios: VocabularyUtils.PREFIX + "has-scenario",
   operationalDataFilter: VocabularyUtils.PREFIX + "has-operational-data-filter",
+  system: VocabularyUtils.PREFIX + "is-artifact-of",
+  subSystem: VocabularyUtils.PREFIX + "is-performed-by",
   name: VocabularyUtils.PREFIX + "name",
   status: VocabularyUtils.PREFIX + "status",
 };
 
-export const CONTEXT = Object.assign({}, ctx, ABSTRACT_CONTEXT, EVENT_CONTEXT, SCENARIO_CONTEXT, FILTER_CONTEXT);
+export const CONTEXT = Object.assign(
+  {},
+  ctx,
+  ABSTRACT_CONTEXT,
+  EVENT_CONTEXT,
+  SCENARIO_CONTEXT,
+  FILTER_CONTEXT,
+  SYSTEM_CONTEXT,
+);
 
 export interface FaultTree extends AbstractModel {
   name: string;
